8f14165de2efa880f44bc7802f969616cc8ff47e
Brilly Majuan <sbmajuanl@gmail.com>
2017-04-11 15:51:48 -0500

    subiendo los cambios en css y js

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+  var contenedor=document.getElementById("contenedor");
+  for (var i = 1; i <= 6; i++) {
+  var div=document.createElement("div");
+  var img=document.createElement("img");
+  var titulo=document.createElement("titulo");
+  div.setAttribute("class","conten-images");
+  img.setAttribute("class","work_img");
+  img.setAttribute("width","320px");
+  img.setAttribute("onclick","onClick(this)")
+  img.setAttribute("src","assets/img/img-"+i+".jpg");
+  titulo.setAttribute("class","titulo");
+  titulo.innerHTML="Nombre del proyecto";
+  div.appendChild(img);
+  div.appendChild(titulo);
+  contenedor.appendChild(div);
+  }
+
+  function onClick(element) {//Muestra los elementos (imagen)//
+    document.getElementById("img_modal").src = element.src;
+    document.getElementById("modal").style.display ="block";
+  }
+  function noonClick() {//Oculta las demas imagenes//
+    document.getElementById("modal").style.display ="none";
+  }
